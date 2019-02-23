1e0a426633bc2942bad568c6a7e64414272c1214
pchq <9292358@gmail.com>
2019-02-24 02:29:46 +0800

    109

---
+++ src/index.js
@@ -0,0 +1,32 @@
+import {createStore} from 'redux';
+
+const reducer = (state = 0, action) => {
+
+    switch (action.type){
+        case 'INC':
+            return state + 1;
+            
+        case 'DEC':
+            return state - 1;
+            
+        default:
+            return state;
+    }
+}
+
+const store = createStore(reducer);
+
+document
+    .getElementById('inc')
+    .addEventListener('click', () => store.dispatch({type: 'INC'}));
+document
+    .getElementById('dec')
+    .addEventListener('click', () => store.dispatch({type: 'DEC'}));
+
+const update = () => {
+    document
+        .getElementById('cnt')
+        .innerHTML = store.getState();
+};
+
+store.subscribe(update);
