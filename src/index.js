c3efd765b905487ac451fa9bb93d8e2e1545458a
pchq <9292358@gmail.com>
2019-02-24 03:10:22 +0800

    112

--- src/index.js
+++ src/index.js
@@ -1,40 +1,24 @@
 import {createStore} from 'redux';
+import reducer from './reducer';
+import {inc, dec, rnd} from './actions';
 
-const reducer = (state = 0, action) => {
-
-    switch (action.type){
-        case 'INC':
-            return state + 1;
-            
-        case 'DEC':
-            return state - 1;
-                        
-        case 'RND':
-            return state + action.payload;
-            
-        default:
-            return state;
-    }
-};
 
 const store = createStore(reducer);
 
+
 document
     .getElementById('inc')
-    .addEventListener('click', () => store.dispatch({type: 'INC'}));
+    .addEventListener('click', () => store.dispatch(inc()));
 document
     .getElementById('dec')
-    .addEventListener('click', () => store.dispatch({type: 'DEC'}));
+    .addEventListener('click', () => store.dispatch(dec()));
 
 document
     .getElementById('rnd')
     .addEventListener('click', 
         () => {
             const payload = Math.floor(Math.random()*10);
-            store.dispatch({
-                type: 'RND', 
-                payload
-            })
+            store.dispatch(rnd(payload))
         }
     );
 
